358e40bad972a21a6adf22c6e356e68627fa3802
carter-sw <6k2hbc@gmail.com>
2024-02-21 22:48:51 +0900

    2.21

---
+++ src/main/java/com/github/supercoding/web/controller/ElectronicStroeController.java
@@ -0,0 +1,100 @@
+package com.github.supercoding.web.controller;
+
+import com.github.supercoding.web.dto.Item;
+import com.github.supercoding.web.dto.ItemBody;
+import org.springframework.web.bind.annotation.*;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/api")
+public class ElectronicStroeController {
+
+    private static int serialItemId =1;
+    private List<Item> items = new ArrayList<>(Arrays.asList(
+            new Item(String.valueOf(serialItemId++),"Apple iPhone 12 Pro Max","Smartphone",1490000,"A14 Bionic","512GB"),
+            new Item(String.valueOf(serialItemId++),"Samsung Galaxy S21 Ultra","Smartphone",1690000,"Exynos 2100","256GB"),
+            new Item(String.valueOf(serialItemId++),"Google Pixel 6 Pro","Smartphone",1290000,"Google Tensor","128GB"),
+            new Item(String.valueOf(serialItemId++),"Dell XPS 15","Laptop",2290000,"Intel Core i9","1TB SSD"),
+            new Item(String.valueOf(serialItemId++),"Sony Alpha 7 III","Mirrorless Camera",2590000,"BIONZ X","No internal storage"),
+            new Item(String.valueOf(serialItemId++),"Microsoft Xbox Series X","Gaming Console",4990000,"Custom AMD Zen 2","1TB SSD")));
+
+    @GetMapping("/items")
+    public List<Item> findAllItem(){
+        return items;
+    }
+
+    @PostMapping("/items")
+    public String registerItem(@RequestBody ItemBody itemBody){
+        Item newItem = new Item(serialItemId++,itemBody);
+        items.add(newItem);
+        return "ID: " +newItem.getId();
+    }
+
+    @GetMapping("/items/{id}")
+    public Item findItemByPathId(@PathVariable String id){
+        Item itemFounded = items.stream()
+                .filter( (item -> item.getId().equals(id)))
+                .findFirst()
+                .orElseThrow( ()->new RuntimeException() );
+
+        return itemFounded;
+    }
+
+    @GetMapping("/items-query")
+    public Item findItemByQueryId(@RequestParam("id") String id){
+        Item itemFounded = items.stream()
+                .filter( (item -> item.getId().equals(id)))
+                .findFirst()
+                .orElseThrow( ()->new RuntimeException() );
+
+        return itemFounded;
+
+    }
+
+    @GetMapping("/items-queries")
+    public List<Item> findItemByQueryIds(@RequestParam("id") List<String> ids){
+
+        Set<String> idset = ids.stream().collect(Collectors.toSet());
+
+        List<Item> itemFound = items.stream()
+                .filter( item -> idset.contains(item.getId())).collect(Collectors.toList());
+
+
+        return itemFound;
+
+    }
+
+    @DeleteMapping("/items/{id}")
+    public String deleteItemByPathId(@PathVariable String id){
+        Item itemFounded = items.stream()
+                .filter( (item -> item.getId().equals(id)))
+                .findFirst()
+                .orElseThrow( ()->new RuntimeException() );
+
+        items.remove(itemFounded);
+        return "Object with id =" +itemFounded.getId() + " has been deleted";
+
+    }
+
+    @PutMapping("/items/{id}")
+    public Item updateItem(@PathVariable String id,@RequestBody ItemBody itemBody){
+        Item itemFounded = items.stream()
+                .filter( (item -> item.getId().equals(id)))
+                .findFirst()
+                .orElseThrow( ()->new RuntimeException() );
+
+        items.remove(itemFounded);
+
+        Item itemUpdated = new Item(Integer.valueOf(id),itemBody);
+        items.add(itemUpdated);
+
+        return itemUpdated;
+    }
+
+}
